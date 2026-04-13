8711d018c645da14605ce035e47f83be9fc577fd
Century322 <zxd1417zxd@gmail.com>
2026-04-14 01:36:24 +0800

    修复了一些问题

--- src/components/mobile/MobileLayout.tsx
+++ src/components/mobile/MobileLayout.tsx
@@ -74,6 +74,7 @@ export function MobileLayout({ settings, matrix, canvasRef, onDownload, onThemeC
 
   const handleTouchStart = (e: React.TouchEvent) => {
     touchStartX.current = e.touches[0].clientX;
+    touchEndX.current = e.touches[0].clientX;
   };
 
   const handleTouchMove = (e: React.TouchEvent) => {
